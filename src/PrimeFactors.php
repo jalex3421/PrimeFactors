30d60fa1e5d2e59e7402d56a5600b8659fa5d6fd
jalex3421 <alemt1708@hotmail.com>
2022-02-24 11:04:43 +0000

    Commit 2: number_1_has_no_factors src

--- src/PrimeFactors.php
+++ src/PrimeFactors.php
@@ -1,10 +1,27 @@
 <?php
 
-
 namespace Deg540\PHPTestingBoilerplate;
-
 
 class PrimeFactors
 {
+    /* se tiene en cuenta si entra 1 ,2 ,3 y 4
+    public funcion calculate(int $number_1_to_Factor):array{
+        return ();
+    } */
+
+    public function calculate(int $number_1_to_Factor):array
+    {
+        if($number_1_to_Factor==1){ return array();}
+        else{return array(1);}
+    }
+
+
+    public function convertPrime(int $number_to_be_converted):string{
+        if($number_to_be_converted ==1){return "not prime";} //if the value is 1, its not prime
+        for($i=2; $i<= intval($number_to_be_converted/2);$i++){ //other case: iterate until half
+            if($number_to_be_converted%$i==0){return "not prime";}
+        }
+        return "prime";
+    }
 
 }
